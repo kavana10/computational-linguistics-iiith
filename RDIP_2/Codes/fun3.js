5dcf1bb8971769476d899e552afb833832b7c93d
kavanareddy10@gmail.com <kavanareddy10@gmail.com>
2020-07-17 19:21:03 +0530

    goal3 for Unit5

--- RDIP_2/Codes/fun3.js
+++ RDIP_2/Codes/fun3.js
@@ -1,2 +1,24 @@
 var corpus = [["The child liked the chocolate.","She was stopped by the bravest knight.","Mary baked a cake for his birthday","She decorated the cake carefully","Mary wore a dress with polka dots."],
 			  ["राम ने सीता के लिए फल तोड़ा।","छोटे बच्चे पाठशाला जल्दी आयेंगे।","मेहनत का फल मीठा होता है।","वाह! वह खूबसूरत है।","पेड़ से पत्ते गिर गए।"]];
+
+
+let selection = document.getElementById("choose");
+
+function dropdown(){
+ if(selection.value == "English"){
+        document.getElementById('english').style.display = "block";
+     document.getElementById('hindi').style.display = "none";
+       
+    }
+    else if(selection.value == "Hindi"){
+        document.getElementById('english').style.display = "none";
+        document.getElementById('hindi').style.display = "block";
+    }
+    else if(selection.value == "default"){
+        document.getElementById('hindi').style.display = "none";
+        document.getElementById('english').style.display = "none";
+        
+    }
+
+
+}
